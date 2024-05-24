1cf43bd8bd1db834bfb6c8aa84f06c34d6d74171
Euller Peixoto <epeixoto.dev@gmail.com>
2024-05-24 11:51:24 -0300

    handle cors

---
+++ src/application/hooks/Cors.php
@@ -0,0 +1,19 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Cors {
+  public function enableCors() {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+    $CI =& get_instance();
+
+    $method = $CI->input->server('REQUEST_METHOD');
+    
+    if ($method == "OPTIONS") {
+      die();
+    }
+  }
+}
