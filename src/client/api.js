20364d90ec7cedea0109d56d536176f3dd0db96d
Euller Peixoto <epeixoto.dev@gmail.com>
2024-05-24 13:48:47 -0300

    setup axios

---
+++ src/client/api.js
@@ -0,0 +1,3 @@
+const API_URL = "http://localhost/api";
+
+export { API_URL };
--- src/client/src/main.js
+++ src/client/src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueAxios from "vue-axios";
+import axios from "axios";
 
-Vue.config.productionTip = false
+import { API_URL } from "../api";
+
+Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
+
+axios.defaults.baseURL = API_URL;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
