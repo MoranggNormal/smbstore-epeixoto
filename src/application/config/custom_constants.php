74ee7c081d8fb52a786008d46fdbab71030743ea
Euller Peixoto <epeixoto.dev@gmail.com>
2024-05-25 09:39:12 -0300

    allow local client for using cookies

--- src/application/config/custom_constants.php
+++ src/application/config/custom_constants.php
@@ -1,6 +1,10 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 $config['auth'] = array(
     'LOGGED_USER'                => 'logged_user',
+);
+
+$config['host'] = array(
+    'CLIENT_HOST' => getenv('CLIENT_HOST')
 );
--- src/application/hooks/Cors.php
+++ src/application/hooks/Cors.php
@@ -4,10 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Cors {
   public function enableCors() {
-    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin: " . config_item('host')['CLIENT_HOST']);
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-    header("Access-Control-Allow-Headers: Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token");
-
+    header("Access-Control-Allow-Headers: Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, ");
+    header("Access-Control-Allow-Credentials: true");
+    
     $CI =& get_instance();
 
     $method = $CI->input->server('REQUEST_METHOD');
