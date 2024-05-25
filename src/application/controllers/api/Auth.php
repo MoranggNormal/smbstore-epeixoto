02bf4247e76a599a0c55da507c38c853b6b73660
Euller Peixoto <epeixoto.dev@gmail.com>
2024-05-25 09:58:14 -0300

    enhanced auth handler semantics

--- src/application/controllers/api/Auth.php
+++ src/application/controllers/api/Auth.php
@@ -5,17 +5,46 @@ class Auth extends CI_Controller
 {
     private $user_session;
 
+    /**
+     * Constructor for Auth class.
+     * Initializes the parent constructor, loads necessary models, helpers, and libraries.
+     * Also, retrieves user session data.
+     */
     public function __construct()
     {
+        /**
+         * Calls the parent constructor of CI_Controller.
+         */
         parent::__construct();
 
+        /**
+         * Loads the UserModel.
+         */
         $this->load->model('UserModel');
+
+        /**
+         * Loads the validation helper.
+         */
         $this->load->helper('validation');
+        /**
+         * Loads the form_validation library.
+         */
+
         $this->load->library('form_validation');
 
+        /**
+         * Retrieves user session data.
+         *
+         * @var mixed $this->user_session User session data.
+         */
         $this->user_session = $this->session->userdata($this->user_session_data);
     }
 
+    /**
+     * Checks if the user is already logged in.
+     *
+     * @return void
+     */
     public function index()
     {
         if ($this->user_session) {
@@ -34,6 +63,20 @@ class Auth extends CI_Controller
             'password' => $this->input->post('password')
         );
 
+        $this->login($data['email'], $data['password']);
+    }
+
+    /**
+     *
+     * Validates the user's login credentials and logs them in.
+     *
+     * @param string $email The user's email address.
+     * @param string $password The user's password.
+     * 
+     * @return void
+     */
+    private function login(string $email, string $password)
+    {
         $this->form_validation->set_rules(
             'email',
             'Email',
@@ -52,8 +95,8 @@ class Auth extends CI_Controller
         }
 
         $logged_user = $this->UserModel->login(
-            $data['email'],
-            $data['password']
+            $email,
+            $password
         );
 
         if ($logged_user) {
@@ -77,6 +120,11 @@ class Auth extends CI_Controller
         }
     }
 
+    /**
+     * Logs the user out and removes their session data.
+     *
+     * @return void
+     */
     public function logout()
     {
         if ($this->user_session) {
@@ -99,6 +147,11 @@ class Auth extends CI_Controller
         }
     }
 
+    /**
+     * Checks if the user is currently logged in and returns their session data.
+     *
+     * @return void
+     */
     public function checkSession()
     {
         if ($this->user_session) {
