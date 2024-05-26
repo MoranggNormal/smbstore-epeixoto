71f0643a0197dc25b13a4b309ff65f4f90351375
Euller Peixoto <epeixoto.dev@gmail.com>
2024-05-26 17:11:17 -0300

    hardcore serverhost

--- src/client/constants.js
+++ src/client/constants.js
@@ -1,3 +1,3 @@
-export const SERVER_HOST = "http://localhost/";
+export const SERVER_HOST = "https://smb.pixellabo.com.br/";
 
 export const IMAGE_NOT_FOUND_URL = 'https://placehold.co/96x96?text=Imagem+não+encontrada'
